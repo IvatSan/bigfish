b07d93ef940c5be57ea058e478895a76731e5419
eu, too <eu_too@yahoo.com>
2016-02-24 10:57:24 -0500

    comit - dev branch

--- bigfish-app/src/main/java/com/doit/bigfish/App.java
+++ bigfish-app/src/main/java/com/doit/bigfish/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Wellcome to BigFish Application !" );
+        System.out.println( "Wellcome to BigFish Application ! - DEV" );
     }
 }
